3eaf98a291a3210223ebf544102947860cb365be
Chris Soncrant <christopher.soncrant@gmail.com>
2022-05-11 12:10:13 -0700

    uncommented necessary function

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function selectMediaStream(displayMediaOptions) {
 
 }
 
-// selectMediaStream();
+selectMediaStream();
 
 button.addEventListener('click', async () => {
     button.disabled = true;
